272b04f3472452507f30f3c5b7f299c3ce1208af
KentPanlaan <kentdarilpanlaan@gmail.com>
2024-09-25 20:17:07 +0800

    Add files via upload

--- PANLAAN/new.php
+++ PANLAAN/new.php
@@ -1,108 +1,128 @@
-<!doctype html>
+<?php
+require_once('connection.php');
+?>
+
+<!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Bootstrap demo</title>
-  <link rel="stylesheet" href="style.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-</head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" >
 
+</head>
 <body>
-  <div class="container mt-5">
-    <form class="row g-3">
-      <div class="row">
-        <div class="col">
-        <label for="inputFirstname4" class="form-label">Firstname</label>
-          <input type="text" class="form-control"  aria-label="First name">
-        </div>
-        <div class="col">
-        <label for="inputLastname4" class="form-label">Lastname</label>
-          <input type="text" class="form-control"  aria-label="Last name">
-        </div>
-      </div>
-      <div class="col-md-6">
-        <label for="inputEmail4" class="form-label">Email</label>
-        <input type="email" class="form-control" id="inputEmail4">
-      </div>
-      <div class="col-md-4">
-    <label for="inputState" class="form-label">Gender</label>
-    <select id="inputState" class="form-select">
-      <option selected>Choose...</option>
+
+<?php
+
+$newconnection->addStudent();
+?>
+
+  <div class="container">
+  <form action="" method="POST" class="row g-12">
+<div class="row">
+  
+    <!-- <form class="row g-3"> -->
+    
+    <div class="col">
+    <label for="inputEmail4" class="form-label"> First name</label>
+    <input type="text" class="form-control" id="first_name" name="first_name"required>
+  </div>
+  <div class="col">
+  <label for="inputEmail4" class="form-label"> Last name</label>
+    <input type="text" class="form-control" id="last_name" name="last_name" required>
+  </div>
+   <div class="col">
+    <label for="inputEmail4" class="form-label">Email</label>
+    <input type="email" class="form-control" id="email" name="email" required>
+  </div>
+
+</div>
+<div class="row">
+  
+    <!-- <form class="row g-3"> -->
+    
+    <div class="col">
+    <label for="inputGender" class="form-label">Gender</label>
+    <select id="gender" class="form-select" id="gender" name="gender"required>
       <option>Male</option>
       <option>Female</option>
     </select>
   </div>
-      <div class="col-md-6">
-        <label for="inputBirthdate" class="form-label">Birthdate</label>
-        <input type="date" class="form-control" id="inputBirthdate">
-      </div>
-      <div class="col-md-6">
-        <label for="inputAddress" class="form-label">Address</label>
-        <input type="text" class="form-control" id="inputAddress">
-      </div>
-      <div class="col-12 d-flex justify-content-center">
-      <button type="submit" class="btn btn-primary custom-btn mb-5 mt-5">ADD</button>
-      </div>
-    </form>
+  <div class="col">
+  <label for="inputBirth" class="form-label"> Birthdate</label>
+    <input type="date" class="form-control" id="birtdate" name="birthdate"required>
+  </div>
+   <div class="col">
+   <label for="inputAddress" class="form-label">Address</label>
+   <input type="text" class="form-control" id="address" name="address" placeholder="1234 Main St"required>
   </div>
 
-  <div class="form-container p-5 bg-light rounded shadow">
-    <table class="table table-primary">
-      <thead>
-        <tr>
-          <th scope="col"></th>
-          <th scope="col">First_Name</th>
-          <th scope="col">Last_Name</th>
-          <th scope="col">Email</th>
-          <th scope="col">Gender</th>
-          <th scope="col">Address</th>
-          <th scope="col">Birthdate</th>
-          
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td colspan="2">Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td colspan="2">Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </table>
+</div>
+    <div class="col-12">
+      <br>
+      <button type="submit" class="btn btn-primary" name="addstudent">Add</button>
     </div>
-  </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+</form>
+<br>
+<table class="table table-primary">
+  <thead>
+    <tr>
+      <th scope="col"># </th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Email</th>
+      <th scope="col">Gender</th>
+      <th scope="col">Birthdate</th>
+      <th scope="col">Address</th>
+      <th scope="col">Action</th>
+      <th scope="col"> </th>
+
+    </tr>
+  </thead>
+  <tbody>
+  <?php
+//fetch data in the table from database
+
+//get connection
+
+$connection = $newconnection->openConnection();
+
+//prepare statement
+$stmt = $connection->prepare("SELECT * FROM new_tables");
+
+//execute query
+$stmt->execute();
+
+//fetch result of the query
+$result =$stmt->fetchAll();
+
+if($result){
+  foreach($result as $row){
+  ?>
+
+  <tr>
+<td><?php echo $row->id?></td> 
+<td><?php echo $row->first_name?></td>
+<td><?php echo $row->last_name?></td>
+<td><?php echo $row->email?></td>
+<td><?php echo $row->gender?></td>
+<td><?php echo $row->birthdate?></td>
+<td><?php echo $row->address?></td>
+<td><a href="" class="btn btn-warning">Edit</a></td>
+<td><button type="submit" class="btn btn-danger">Delete</button>
+</td>
+</tr>
+<?php
+}
+}
+?>
+  </tbody>
+</table>
+</div>
 </body>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </html>
 cdn.jsdelivr.net
